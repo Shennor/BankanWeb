7c4a230639f3ae3844ff030d834c85b4dd2bb8d3
hupu <krifwerde@gmail.com>
2022-05-11 23:00:36 +0300

    made card edition and deletion requests (but server does not delete card)

--- src/controllers/CardController.ts
+++ src/controllers/CardController.ts
@@ -1,4 +1,3 @@
-import react from "react";
 import {CardInstance} from "./axios.instances";
 import {ErrorType, IError} from "./errors";
 
@@ -17,6 +16,33 @@ export const createCard = async (listId: number, name: string, content?: JSON) =
         return resp.status == 200
     } catch (e) {
         console.error(e)
+        return undefined
+    }
+}
+
+export const deleteCard = async (cardId: number) => {
+    try {
+        const res = await CardInstance.delete(`${cardId}`)
+        if(res.status == 401) return {errorType: ErrorType.unauthorized} as IError
+        return res.status == 200
+    } catch (error) {
+        console.error(error)
+        return undefined
+    }
+}
+
+export const editCard = async (cardId: number, name: string = "") => {
+    try {
+        const res = await CardInstance.patch(`edit/${cardId}`,
+            {
+                name: name
+            })
+        if(res.status == 401)
+            return {errorType: ErrorType.unauthorized} as IError
+        return res.status == 200
+    }
+    catch(error) {
+        console.error(error)
         return undefined
     }
 }

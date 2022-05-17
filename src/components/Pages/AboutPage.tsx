52bc9981799276987516940e1cb3e81f4ada4674
hupu <krifwerde@gmail.com>
2022-05-17 12:23:41 +0300

    About Page corrections

--- src/components/Pages/AboutPage.tsx
+++ src/components/Pages/AboutPage.tsx
@@ -13,11 +13,12 @@ export const AboutPage = () => {
                     <img src={BigLogo}/>
                 </div>
                 <div className={"about"}>
-                    We are Aleksandrov Nikita and Sharipova Elvira - MEPhI students. This site represents our 2nd year course project in Software Engineering.
+                    <p>We are Aleksandrov Nikita and Sharipova Elvira - MEPhI students. This site represents our 2nd year course project in Software Engineering.
                     The server is written by both of us. The client part is separate: website - Elvira, android application - Nikita.
                     We used:
                     server - Kotlin, Spring framework, MySql
                     website - Typescript, React
+                    </p>
                 </div>
             </div>
         </div>
